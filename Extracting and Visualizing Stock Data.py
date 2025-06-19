081f5075f1b0e0f19301ce915dc9dde37665b254
Sadaf Munir <99748543+999sadaf@users.noreply.github.com>
2025-06-20 01:19:51 +0500

    Create Extracting and Visualizing Stock Data.py

---
+++ Extracting and Visualizing Stock Data.py
@@ -0,0 +1,2 @@
+This project is based on  extracting and visualizing data of Historical Share Price and Revenue for two companies, Tesla and GameStop. 
+In this project following python liraries are used: yfinance, requests, pandas, BeautifulSoup and plotly.graph_objects.
